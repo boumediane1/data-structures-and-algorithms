1af5b5c7d74bc1402c5871134e1b879d207cb31f
Othmane Boumediane <boumediane.othmane@gmail.com>
2022-10-30 00:45:22 +0100

    first commit

---
+++ src/main/java/tech/othmane/linkedlist/LinkedList.java
@@ -0,0 +1,69 @@
+package tech.othmane.linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    static class Node {
+        int value;
+        Node next;
+
+        public Node (int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public LinkedList (int value) {
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    public void printList () {
+        Node temp = this.head;
+
+        while (temp != null) {
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+
+    public void append (int value) {
+        Node newNode = new Node(value);
+        if (this.length == 0) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+        this.length++;
+    }
+
+    public boolean removeLast () {
+        if (this.length == 0) {
+            return false;
+        } else if (this.length == 1) {
+            this.head = null;
+            this.tail = null;
+            return true;
+        }
+
+        Node temp = this.head;
+        while (temp.next != this.tail) {
+            temp = temp.next;
+        }
+
+        this.tail = temp;
+        this.tail.next = null;
+        return true;
+    }
+}
---
+++ src/main/java/tech/othmane/linkedlist/Main.java
@@ -0,0 +1,13 @@
+package tech.othmane.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(1);
+        linkedList.append(2);
+        linkedList.append(3);
+
+        linkedList.removeLast();
+
+        linkedList.printList();
+    }
+}
