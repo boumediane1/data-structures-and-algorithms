047d4478725f387b2b64de6eed79079cf72e5abb
Othmane Boumediane <boumediane.othmane@gmail.com>
2023-02-27 15:55:56 +0100

    feat: add removeFirst method to DoublyLinkedList

--- src/main/java/tech/othmane/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/tech/othmane/doublylinkedlist/DoublyLinkedList.java
@@ -73,6 +73,24 @@ public class DoublyLinkedList {
         this.length++;
     }
 
+    public Node removeFirst() {
+        if (this.length == 0) return null;
+
+        Node temp = this.head;
+
+        if (this.length == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            temp.next = null;
+        }
+
+        this.length--;
+        return temp;
+    }
+
     @Override
     public String toString() {
         return "DoublyLinkedList{\n" +
--- src/main/java/tech/othmane/doublylinkedlist/Main.java
+++ src/main/java/tech/othmane/doublylinkedlist/Main.java
@@ -3,10 +3,8 @@ package tech.othmane.doublylinkedlist;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(1);
-        doublyLinkedList.removeLast();
-        // doublyLinkedList.append(2);
-        // doublyLinkedList.append(3);
-        doublyLinkedList.prepend(0);
-        System.out.println(doublyLinkedList);
+        doublyLinkedList.append(2);
+        doublyLinkedList.append(3);
+        System.out.println(doublyLinkedList.removeFirst());
     }
 }
